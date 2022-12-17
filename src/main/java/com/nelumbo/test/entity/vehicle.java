11d4b5930006e604c682f680b27d26c2c0619bbe
BrayanGuerreroXD <bg7047653@gmail.com>
2022-12-17 16:03:44 -0500

    Update vehicle.java

--- src/main/java/com/nelumbo/test/entity/vehicle.java
+++ src/main/java/com/nelumbo/test/entity/vehicle.java
@@ -2,7 +2,16 @@ package com.nelumbo.test.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 
 import java.sql.Date;
 
