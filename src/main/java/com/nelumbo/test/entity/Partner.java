c3d55264519ef98720cc053a7b3b1f74612b7a66
BrayanGuerreroXD <bg7047653@gmail.com>
2022-12-17 16:12:11 -0500

    Update Partner.java

--- src/main/java/com/nelumbo/test/entity/Partner.java
+++ src/main/java/com/nelumbo/test/entity/Partner.java
@@ -1,4 +1,25 @@
 package com.nelumbo.test.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="partner")
+@Data
 public class Partner {
+
+    @Id
+    @SequenceGenerator(name="inscription_id_seq", allocationSize=1)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="inscription_id_seq")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
 }
