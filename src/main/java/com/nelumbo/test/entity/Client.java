8bb812c524055d005ec6af8c92a9266e92aa23da
BrayanGuerreroXD <bg7047653@gmail.com>
2022-12-17 16:12:03 -0500

    Update Client.java

--- src/main/java/com/nelumbo/test/entity/Client.java
+++ src/main/java/com/nelumbo/test/entity/Client.java
@@ -1,4 +1,25 @@
 package com.nelumbo.test.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="client")
+@Data
 public class Client {
+
+    @Id
+    @SequenceGenerator(name="inscription_id_seq", allocationSize=1)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="inscription_id_seq")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
 }
