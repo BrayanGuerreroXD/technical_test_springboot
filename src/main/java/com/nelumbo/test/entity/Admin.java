754f53ce56dff5b9febcd129c9be8437ca416d6c
BrayanGuerreroXD <bg7047653@gmail.com>
2022-12-17 16:12:01 -0500

    Update Admin.java

--- src/main/java/com/nelumbo/test/entity/Admin.java
+++ src/main/java/com/nelumbo/test/entity/Admin.java
@@ -1,4 +1,28 @@
 package com.nelumbo.test.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admin")
+@Data
 public class Admin {
+
+    @Id
+    @SequenceGenerator(name="inscription_id_seq", allocationSize=1)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="inscription_id_seq")
+    private Integer id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
 }
